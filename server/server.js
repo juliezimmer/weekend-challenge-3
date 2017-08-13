fb28f7b743eaa216fcd6bbcb9fb008144fcfc6be
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-08-13 12:59:53 -0500

    server is up and running

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+//var path = require( 'path' );
+var port = process.env.PORT || 3000;
+var tasks = require('./routes/routes');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+//app.use('/tasks',tasks);
+
+// listen for requests on a specific port
+app.listen(port, function(){
+    console.log("listening on port:", port);
+});
