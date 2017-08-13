120c34da70441293da07995b5423dee3bd428938
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2017-08-13 14:12:08 -0500

    created GET request to access DB; router.get also created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,22 @@ console.log("the clinet.js file is linked to the index.html file");
 
 $(document).ready(function(){
     console.log("jquery successfully locked and loaded");
+
+    //load existing tasks when the page loads
+    getTasks();
 });
+
+
+//get the tasks from the DB to load on the DOM
+function getTasks(){
+    console.log("in getTasks");
+    //ajax method call to server/DB to get tasks
+    $.ajax({
+        type: "GET",
+        url: "/tasks",
+        success: function(res) {
+            console.log("Here is the current task list:", res);
+            createTaskList();
+        } //end of ajax call
+    });     
+} //end getTasks() function
